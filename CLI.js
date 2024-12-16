8712044cc9ca3261358ff07e095c2e488e1b8956
Milton Bolonha <miltonbolonha@gmail.com>
2024-12-16 11:19:19 -0300

    bugfix/build errors

--- CLI.js
+++ CLI.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
+const rootFolder = require("app-root-path");
+const contentFolder = rootFolder + `/content`;
+console.log(`${contentFolder}/settings/version`);
 require("./core/index");
